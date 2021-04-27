572e65d8dcffdfafe0c3ec3cbefa067f1add652b
sivtcev <sivtcev@gmail.com>
2021-04-27 14:19:24 +0300

    initial commit

---
+++ src/main/java/com/sivtcev/distance/DistanceCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.sivtcev.distance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistanceCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistanceCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sivtcev/distance/DistanceCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sivtcev.distance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistanceCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
